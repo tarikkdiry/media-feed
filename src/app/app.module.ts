0bc5a5671dc07ecd824cc0e27fadeb34d7d13afe
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-06-25 10:05:52 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+// Material
+import { MaterialModule } from '@angular/material';
+
+// Routes
+// import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    // RouterModule.forRoot(ROUTES),
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

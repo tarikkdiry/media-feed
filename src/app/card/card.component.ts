d298327c9736dd6a0c784608363d1e1d43a84449
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-06-26 11:25:13 -0400

    added SharePoint CRUD Operations

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   test: boolean;
+  // tslint:disable-next-line:no-inferrable-types
   title: string = 'Video Title';
+  // tslint:disable-next-line:no-inferrable-types
   postedOn: string = '01/01/2018'; // Probably a string
+
   description: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
   constructor() { }
 
